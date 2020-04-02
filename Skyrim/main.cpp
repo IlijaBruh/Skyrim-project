27a85d5fdb01329faffd59ea2a3ef75c2d96d6d3
IlijaBruh <krivokuca.ilija@jjzmaj.edu.rs>
2020-04-02 17:22:54 +0200

    Added stuf
    started with spells

--- Skyrim/main.cpp
+++ Skyrim/main.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+class spell{
+private:
+    string name;
+    int dmg;
+    int drain;
+
+};
+
 int main()
 {
     cout << "i used to be an adventurer like you until i took an arrow to the knee" << endl;
+
     return 0;
 }
