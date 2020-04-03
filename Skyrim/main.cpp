1ece1041f45eef776bd35cdfb411b53e37fc86b1
IlijaBruh <krivokuca.ilija@jjzmaj.edu.rs>
2020-04-03 19:06:24 +0200

    Player
    Added class Player

--- Skyrim/main.cpp
+++ Skyrim/main.cpp
@@ -3,31 +3,37 @@
 using namespace std;
 
 class spell{
-private:
-    string sname;
-    float sdmg;
-    int sdrain;
+    private:
+        string sname;
+        float sdmg;
+        int sdrain;
 
-public:
-    void setName(string sna){
-        sname = sna;
-    }
-    void setDmg(float sdm){
-        sdmg = sdm;
-    }
-    void setDrai(int sdr){
-        sdrain = sdr;
-    }
+    public:
+        void setName(string sna){
+            sname = sna;
+        }
+        void setDmg(float sdm){
+            sdmg = sdm;
+        }
+        void setDrai(int sdr){
+            sdrain = sdr;
+        }
 
-    string getSname(){
-        return sname;
-    }
-    float getSdmg(){
-        return sdmg;
-    }
-    int getSdrain(){
-        return sdrain;
-    }
+        string getSname(){
+            return sname;
+        }
+        float getSdmg(){
+            return sdmg;
+        }
+        int getSdrain(){
+            return sdrain;
+        }
+
+};
+
+class Player{
+    private:
+        string name;
 
 };
 
