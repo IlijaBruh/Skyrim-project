b2f696132e14538f054021518f27bc1c14a6eddf
IlijaBruh <krivokuca.ilija@jjzmaj.edu.rs>
2020-05-06 15:00:46 +0200

    Falmer
    Falmer geter, falmer seter

--- Skyrim/main.cpp
+++ Skyrim/main.cpp
@@ -282,6 +282,44 @@ class spider : monster{
         }
 };
 
+class falmer : monster{
+    private:
+        string type;
+    public:
+        void setType(int ty){
+            switch(ty){
+
+                case 1:
+                    type = "Archer";
+                    break;
+
+                case 2:
+                    type = "Shaman";
+                    break;
+
+                case 3:
+                    type = "Skulker";
+                    break;
+
+                case 4:
+                    type = "Shadowmaster";
+                    break;
+
+                case 5:
+                    type = "Nightprowler";
+                    break;
+
+                case 6:
+                    type = "Gloomlurker";
+                    break;
+            }
+        }
+
+        string getType(){
+            return type;
+        }
+};
+
 
 
 class Player{
